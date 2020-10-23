73d30e7f5c72c4ad71e7a875ad338b72556733d1
Alexander <acepetrov96@gmail.com>
2020-10-22 19:33:29 -0500

    commit tree 83e60548a248e7948a83574af921e10804840a24

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+dline = []
+
+def line(dline)
+    if dline.length == 0
+        puts "The line is currently empty."
+    else
+        arr = ["The line is currently: "]
+        dline.each.with_index(1) do |customer, index|
+            arr << "#{index}. #{customer} "
+        end 
+        puts arr.join.strip
+    end
+end
+
+def take_a_number(dline, name)
+    dline << name
+    puts "Welcome, #{dline[dline.size-1]}. You are number #{dline.length} in line."
+end
+
+def now_serving(dline)
+    if dline.size == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{dline.shift}."
+    end
+end
